f93d4776fc073da241e52ca559fa3aadb90ec617
Ana3815 <116765150+Ana3815@users.noreply.github.com>
2023-08-29 22:31:24 -0500

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,15 @@ import { verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 //ruta de registrar
-
+router.use(register);
 /**GUARDAR LOS DATOS DE LAS ELECCIONES */
-router.post("/register", register); /**traer el controlador */
+router.post("/register", register); 
+/**traer el controlador */
 router.post("/login", login); /**traer el controlador */
 // router.get("/verifyToken", verifyToken); /**ruta de verificar token */
-
 router.use(verifyToken);
+
+
 
 router.get("/verifyToken", (req, res) => {
     res.status(200).json({ message: "Token válido" });
